e99a141f8986cd781b4d574f1ccf09b2fd2cee2c
Boris Nicolai <entwicklung@boris-nicolai.de>
2025-12-27 19:21:22 +0100

    Add Isar storage provider and job repository

---
+++ lib/storage/isar_provider.dart
@@ -0,0 +1,28 @@
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
+import '../models/scan_job.dart';
+
+class IsarProvider {
+  IsarProvider._internal();
+
+  static final IsarProvider _instance = IsarProvider._internal();
+
+  factory IsarProvider() => _instance;
+
+  Isar? _isar;
+
+  Future<Isar> get instance async {
+    if (_isar != null && _isar!.isOpen) {
+      return _isar!;
+    }
+
+    final dir = await getApplicationDocumentsDirectory();
+    _isar = await Isar.open(
+      schemas: [ScanJobSchema],
+      directory: dir.path,
+    );
+
+    return _isar!;
+  }
+}
