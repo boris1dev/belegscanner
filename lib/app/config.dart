ab7206a65dfc5ccf7c1deeeb912896cd33d9bc29
Boris <mail@boris-nicolai.de>
2025-12-30 14:03:20 +0100

    fixed api url

--- lib/app/config.dart
+++ lib/app/config.dart
@@ -13,6 +13,6 @@ class ApiConfig {
 }
 
 const defaultApiConfig = ApiConfig(
-  apiBaseUrl: 'https://n8n.zeitgewinn.ai/webhook/78dc69de-06a3-491e-a19c-715d7a3f15d3',
+  apiBaseUrl: 'https://n8n.zeitgewinn.ai/webhook/belegscanner',
   apiKey: '',
 );
